632d06c07bfab7978a37999caf7bc5ec5c408a76
190736 <190736@e0505-18.JC-21.local>
2020-12-08 15:55:56 +0900

    課題5

--- src/Kadai05Test.java
+++ src/Kadai05Test.java
@@ -8,8 +8,8 @@ class Kadai05Test {
 	@Test
 	void testGetMax() {
 		Kadai05 k = new Kadai05();
-		int [] e = {1,2,3,4,1,12,12,-100};
-		int i1 = k.getMax(e);
+		int [] q = {1,2,3,4,1,12,12,-100};
+		int i1 = k.getMax(q);
 		assertThat(i1,is(12));
 
 	}
