c78334ea0b1d8a62d081927523088ee3de81292f
190736 <190736@e0505-18.JC-21.local>
2020-12-08 16:36:45 +0900

    kadai4

--- src/Kadai04.java
+++ src/Kadai04.java
@@ -1,4 +1,3 @@
-
 public class Kadai04 {
 
 	/**
@@ -8,6 +7,34 @@ public class Kadai04 {
 	 * @return sの中に入っていた文字
 	 */
 	char getMaxString(String s) {
+
+		char a[] = s.toCharArray();
+		int max = 0;
+		char ch = a[0];
+		char cha ;
+		for (int i = 0; i < a.length-1; i++) {
+			int count = 1;
+
+			for (int j = 1; j < a.length; j++) {
+
+				if (a[i] == a[j]) {
+					count++;
+				}
+
+				if (count > max) {
+					if (a[i] < cha) {
+
+					}
+
+				}
+
+			}
+
+		}
+
+
+
+
 		char c = ' ';
 		return c;
 	}
